31f7663a8c403a38721069ab6667a4c47f75edd3
Amornchai Kanokpullwad <amornchai.zoon@gmail.com>
2014-08-03 23:33:28 +0700

    add option for currency, release to store as 0.1.1

--- popup.js
+++ popup.js
@@ -17,11 +17,13 @@ var currencyService = {
     req.send(null);
   },
   convertJPYHandler_: function(e) {
+    document.getElementById('loading').style.display = 'none';
     var response = e.target.response;
     var responseJson = JSON.parse(response);
     var revenue = responseJson.v.toFixed(0);
-    console.log(e.target.period + " revenue in thai = " + revenue);
-    document.getElementById(e.target.period + "Cur").innerHTML = revenue;
+    console.log(e.target.period + " revenue in currency = " + revenue);
+    document.getElementById(e.target.period + "Cur")
+      .innerHTML = numberWithCommas(revenue);
   }
 
 }
@@ -34,6 +36,7 @@ var lineService = {
     req.open("GET", checkLoginUrl, true);
     req.onload = this.checkLoginHandler_.bind(this);
     req.send(null);
+    document.getElementById('loading').style.display = 'block';
   },
   checkLoginHandler_: function(e) {
     var response = e.target.response;
@@ -55,6 +58,7 @@ var lineService = {
       } 
         
       // unexpected error if reached here
+      document.getElementById('loading').style.display = 'none';
 
     } else {
       // open login tab
@@ -70,6 +74,7 @@ var lineService = {
     req.send(null);
   },
   getRevenueHandler_: function(e) {
+
     var response = e.target.response;
     var responseHtml = new DOMParser().parseFromString(response, 'text/html');
 
@@ -93,17 +98,100 @@ var lineService = {
 
     console.log("today revenue = " + todayRevenue);
     console.log("this month revenue = " + monthRevenue);
-    document.getElementById("todayJPY").innerHTML = todayRevenue;
-    document.getElementById("monthJPY").innerHTML = monthRevenue;
 
-    // convert currency
-    currencyService.convertJPY("THB", todayRevenue, "today");
-    currencyService.convertJPY("THB", monthRevenue, "month");
+    var currency = document.getElementById('cur-select').value;
+    if (currency != "JPY") {
+      document.getElementById("todayJPY")
+        .innerHTML = numberWithCommas(todayRevenue);
+      document.getElementById("monthJPY")
+        .innerHTML = numberWithCommas(monthRevenue);
+      currencyService.convertJPY(currency, todayRevenue, "today");
+      currencyService.convertJPY(currency, monthRevenue, "month");
+    } else {
+      document.getElementById('loading').style.display = 'none';
+      document.getElementById("todayCur")
+        .innerHTML = numberWithCommas(todayRevenue);
+      document.getElementById("monthCur")
+        .innerHTML = numberWithCommas(monthRevenue);
+    }
   }
 
 }
 
+// utils
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// Saves options to chrome.storage
+function save_options() {
+  var currency = document.getElementById('cur-select').value;
+  chrome.storage.sync.set({
+    currency: currency
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options(callback) {
+  // Use default value currency = 'JPY'
+  chrome.storage.sync.get({
+    currency: 'JPY'
+  }, function(items) {
+    document.getElementById('cur-select').value = items.currency;
+    callback();
+  });
+}
+
+// layout
+function layoutUI() {
+  // remove unused element if currency is JPY
+  var currency = document.getElementById('cur-select').value;
+  document.getElementById('todayCur-unit').innerHTML = currency;
+  document.getElementById('monthCur-unit').innerHTML = currency;
+  if (currency == "JPY") {
+    document.body.parentNode.style.height = "300px";
+    document.body.style.height = "300px";
+    document.getElementById('today-sub').style.display = 'none';
+    document.getElementById('month-sub').style.display = 'none';
+  } else {
+    document.getElementById('today-sub').style.display = 'block';
+    document.getElementById('month-sub').style.display = 'block';
+  }
+}
+
 // Run cript as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
-  lineService.checkLogin();
+
+  document.getElementById('ads').onclick = function() {
+    chrome.tabs.create(
+      {url: "https://store.line.me/stickershop/product/1001268/"}
+    );
+  }
+
+  // register on currency change
+  document.getElementById('cur-select').onchange = function() {
+    document.getElementById("todayCur").innerHTML = "-";
+    document.getElementById("monthCur").innerHTML = "-";
+
+    save_options();
+    layoutUI();
+    lineService.checkLogin();
+  }
+
+  restore_options(function() {
+    layoutUI();
+    lineService.checkLogin();
+  });
+  
 });
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-44460572-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
