69db7052acdab9e6f664657a6e9898115ce123d0
Amornchai Kanokpullwad <amornchai.zoon@gmail.com>
2014-08-04 22:50:06 +0700

    fix some line over 80 column

--- popup.js
+++ popup.js
@@ -191,7 +191,9 @@ _gaq.push(['_setAccount', 'UA-44460572-2']);
 _gaq.push(['_trackPageview']);
 
 (function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; 
+  ga.async = true;
   ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  var s = document.getElementsByTagName('script')[0]; 
+  s.parentNode.insertBefore(ga, s);
 })();
